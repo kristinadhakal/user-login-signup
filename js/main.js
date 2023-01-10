798de47b4f89193f9ea56b1f2e4e63041f74a1df
kristinadhakal <112728281+kristinadhakal@users.noreply.github.com>
2023-01-10 14:44:08 -0700

    1/10/23

--- js/main.js
+++ js/main.js
@@ -1,33 +1,53 @@
 // USER LOGIN / SIGNUP
 
+//
+
 // HTML VARIABLES
 let userEL = document.getElementsByClassName("input-label");
 let signInBtn = document.getElementById("sign-in-btn");
 let signUpBtn = document.getElementById("sign-up-btn");
 
 // Global Variable
-let user = [];
+let user = initUser();
 
 // SIGN UP BTN CLICKED
 signUpBtn.addEventListener("click", signUpHandler);
 
 function signUpHandler() {
   user.innerHTML = "";
+  returnObject(name, password);
 
-for (let i = 0; i < user.length; i++) {
-  if (user[i].includes(userEl.value))
-  console.log("Username already in Use.")
-} 
-else { 
-  userEL.appendCHild(user[i], i);
-
+  for (let i = 0; i < user.length; i++) {
+    if (user[i].includes(userEl.value)) {
+      console.log("Username already in Use.");
+    } else {
+      userEL.appendChild(user[i], i);
+    }
+  }
 }
-} 
-
 
 // SIGN IN BTN CLICKED
 signInBtn.addEventListener("click", signInHandler);
 
 function signInHandler() {
   console.log("Sign In Btn Clicked");
+}
+
+// helper
+
+// new user
+function returnObject(usernameParameter, passwordParameter) {
+  return {
+    name: usernameParameter,
+    password: passwordParameter,
+  };
+}
+
+function initUser() {
+  let jsonUser = localStorage.getItem("user");
+  return JSON.parse(jsonUser) ?? [];
+}
+
+function saveUser() {
+  localStorage.setItem("user", JSON.stringify(user));
 }
